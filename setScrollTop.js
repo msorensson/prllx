c47030456821d2aeb7491a99be543904ab0d0222
msorensson <martin.sorensson@creuna.se>
2016-04-12 13:56:15 +0200

    setScrollTop always takes a delta instead of an optional absolute scrollTop

--- setScrollTop.js
+++ setScrollTop.js
@@ -1,12 +1,10 @@
 'use strict';
-module.exports = function(newScrollTop, d) {
+module.exports = function(d) {
     var self = this;
-    newScrollTop = newScrollTop || self.scrollTop - d;
+    var newScrollTop = self.scrollTop - d;
 
     if (self.isStarted && newScrollTop !== self.scrollTop) {
-
         self.scrollDirection = self.utils.setScrollDirection(newScrollTop, self.scrollTop);
         self.scrollTop = Math.min(Math.max(newScrollTop, 0), self.scrollLimit);
-
     }
 };
